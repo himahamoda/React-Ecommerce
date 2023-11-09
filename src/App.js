0c63bce0c5adade66af8ff83d4d1762a226fa70f
ibrahim ahmed <133769074+himahamoda@users.noreply.github.com>
2023-11-09 21:55:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Tableware from './component/Tableware/Tableware';
 export function App() {
   let routers = createBrowserRouter([
     {
-      path: '/desa', element: <Layout />, children: [
+      path: '', element: <Layout />, children: [
         { path:'Plantpots', element:<Home/> },
         { path: 'Ceramics', element:<SecHome /> },
         { path: 'chairs', element:<Chairs /> },
